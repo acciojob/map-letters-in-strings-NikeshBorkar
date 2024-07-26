89508f86b3867e6af57d2d466bdb525c6913b487
Nikesh Borkar <99666800+NikeshBorkar@users.noreply.github.com>
2024-07-26 21:52:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function mapLetters(key){
+	let n=key.length
+	let obj={}
+	for(let i=0;i<n;i++){
+		let x=key[i]
+		if(obj[x]==undefined){
+			obj[x]=[i]
+		}else{
+	}
+	}}
